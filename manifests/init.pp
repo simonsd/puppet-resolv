b6b88b26bfef4bfb019bc258888cbb6db4d0820b
Dave Simons <dave@inuits.be>
2011-06-03 15:32:06 +0200

    add first define to manage nameservers Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,11 @@
 class resolv {
-	file { "/etc/resolv.conf":
-		ensure => present,
-		owner => root,
-		group => root,
-		mode => 0644,
-		content => template('resolv.conf.erb'),
+	define file ( $domain, $search, $nameserver ) {
+		file { "/etc/resolv.conf":
+			ensure => present,
+			owner => root,
+			group => root,
+			mode => 0644,
+			content => template('resolv.conf.erb'),
+		}
 	}
 }
