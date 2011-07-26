ee9a2a8f1d0a6b0096d77544129b472102f0fdf7
Dave Simons <dave@inuits.be>
2011-07-26 08:48:39 +0200

    add defaults, nicer syntax
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/define.pp
+++ manifests/define.pp
@@ -1,12 +1,14 @@
-class resolv::define {
-	define nameserver ( $domain, $search, $nameserver ) {
-		file { "resolv.conf":
-			ensure => present,
-			path => "/etc/resolv.conf",
-			owner => root,
-			group => root,
-			mode => 0644,
-			content => template('resolv/resolv.conf.erb'),
-		}
+define nameserver (
+	$domain = "",
+	$search = "",
+	$nameserver = "192.168.1.1"
+) {
+	file { "resolv.conf":
+		ensure => present,
+		path => "/etc/resolv.conf",
+		owner => root,
+		group => root,
+		mode => 0644,
+		content => template('resolv/resolv.conf.erb'),
 	}
 }
